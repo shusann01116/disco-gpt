72b9b59583d81e9ef184516f0bcf185733e87412
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2023-05-13 20:15:45 +0900

    add API Gateway and allow lambda invoked from it

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,32 @@ resource "random_id" "this" {
   byte_length = 8
 }
 
+# HTTP API Gateway
+module "api_gateway" {
+  source  = "terraform-aws-modules/apigateway-v2/aws"
+  version = "~> 2.0"
+
+  name          = "disco-gpt-${random_id.this.id}"
+  protocol_type = "HTTP"
+
+  cors_configuration = {
+    allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
+    allow_methods = ["*"]
+    allow_origins = ["*"]
+  }
+
+  domain_name                 = "${var.subdomain_name}.${var.domain_name}"
+  domain_name_certificate_arn = module.acm.acm_certificate_arn
+
+  integrations = {
+    "GET /" = {
+      lambda_arn             = module.lambda.lambda_function_arn
+      payload_format_version = "2.0"
+      timeout_milliseconds   = 12000
+    }
+  }
+}
+
 # Lambda
 
 module "lambda" {
@@ -31,6 +57,13 @@ module "lambda" {
 
   create_package         = false
   local_existing_package = data.archive_file.lambda_package.output_path
+
+  allowed_triggers = {
+    AllowExecutionFromAPIGateway = {
+      service    = "apigateway"
+      source_arn = "${module.api_gateway.apigatewayv2_api_execution_arn}/*/*"
+    }
+  }
 }
 
 resource "null_resource" "build" {
@@ -60,6 +93,21 @@ module "acm" {
   source  = "terraform-aws-modules/acm/aws"
   version = "~> 3.0"
 
-  domain_name = var.domain_name
-  zone_id     = data.aws_route53_zone.this.zone_id
+  domain_name               = var.domain_name
+  zone_id                   = data.aws_route53_zone.this.zone_id
+  subject_alternative_names = ["${var.subdomain_name}.${var.domain_name}"]
+}
+
+# Route53
+
+resource "aws_route53_record" "api" {
+  zone_id = data.aws_route53_zone.this.zone_id
+  name    = var.subdomain_name
+  type    = "A"
+
+  alias {
+    name                   = module.api_gateway.apigatewayv2_domain_name_configuration[0].target_domain_name
+    zone_id                = module.api_gateway.apigatewayv2_domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,7 @@
 variable "domain_name" {
   type = string
 }
+
+variable "subdomain_name" {
+  type = string
+}
